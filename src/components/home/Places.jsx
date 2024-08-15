d9207e25929cc32bfd3c20f1e4e42c71480c959a
AlbertoDoc <albertooliveirasantos@gmail.com>
2024-08-15 17:33:02 -0300

    salvando progresso

--- src/components/home/Places.jsx
+++ src/components/home/Places.jsx
@@ -1,25 +1,43 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import { getAllPlaces } from "../../services/places/getAllPlaces";
 import Header from "./Header"
 import Carousel from "./Carousel"
 import '../../styles/global.css'
 import '../../styles/home.css'
+import NewsGrid from "../news/NewsGrid"
 
 
 /**
- * Componente Places
+ * Componente NewsPage
  *
- * Este componente representa a página de locais.
+ * Este componente representa a página de notícias.
  * 
  */
 
 export default function Places(){
+   const [newsList, setNewsList] = useState([]);
+   const token = localStorage.getItem("token");
 
+   useEffect(() => {
+      loadNews();
+    }, []);
+  
+    function loadNews() {
+      getAllPlaces(token)
+        .then((data) => {
+          console.log(data);
+          setNewsList(data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
 
- return(
-    <>
-    <Header></Header>
-    <Carousel></Carousel>
-    </>
- )
-
+    return(
+        <>
+            <Header></Header>
+            <Carousel newsList={newsList.slice(0, 3)}></Carousel>
+            <NewsGrid newsList={newsList.slice(0, newsList.length)}></NewsGrid>
+        </>
+    )
 }
